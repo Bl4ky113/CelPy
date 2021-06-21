69c2491d8cd386df5c4ad2a9f5b329823cedefa4
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-06-20 20:22:05 -0500

    Agregado enviado mensaje sencillo

--- whatpy/test_pywhatkit.py
+++ whatpy/test_pywhatkit.py
@@ -1,1 +1,17 @@
 # Made By Bl4ky113
+
+import pywhatkit
+from datetime import datetime
+
+celNumber = input("Ingrese su Celular:  ")
+currentTime = {
+  "time": datetime.now(),
+  "hour": datetime.now().hour,
+  "min": datetime.now().minute,
+  "sec": datetime.now().second
+}
+try:
+  pywhatkit.sendwhatmsg(celNumber, "Quiubo", currentTime["hour"], currentTime["min"] + 1)
+
+except:
+  print("No se pudo contactar a su Número")

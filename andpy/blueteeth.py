11601383213237f194154da582fc4f016e0f1d78
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-06-24 14:40:44 -0500

    Arreglado error en comand: change name | agregado command: close

--- andpy/blueteeth.py
+++ andpy/blueteeth.py
@@ -10,29 +10,30 @@ else:
   btConectedDevice = False
 
 # Get the Bluetooth Name of the Device
-def getBtLocalName() -> str:
+def getBtLocalName():
   btLocalName = list(droid.bluetoothGetLocalName())
   btLocalName = str(btLocalName[1])
   return btLocalName
 
 # Show Bluetooth Name of the Device
 def showBtLocalData():
-
   print("|".center(45, "="), "\n")
   print("Device's Name:  ", getBtLocalName(), "\n")
   print("|".center(45, "="))
 
 # Change Bluetooth Name of the Device
-def changeBTLocalName(localName = ""): 
+def changeBTLocalName(localName): 
   answer = input("You want to change the Device's name? (y/n)")
   if answer.casefold() == "y" or answer.casefold() == "yes":
     new_localName = input("Enter your new Name:  ")
     if not new_localName == localName:
-      droid.bluetoothSetLocalName()
-      showBtLocalData()
+      droid.bluetoothSetLocalName(new_localName)
+      print("Now the Device's name is:  ", new_localName)
     else:
       print("That is your current Device's Name")
       changeBTLocalName(localName)
+  else:
+    pass
 
 """ Start the Program """
 
@@ -53,9 +54,17 @@ showBtLocalData()
 
 """ Main Menu """
 while True:
-  print("".center(45, "="), "\n")
   command = input("Enter a Command:  ")
-  print("".center(45, "="), "\n")
 
-  if command.casefold == "change name":
+  if command == "show data":
+    showBtLocalData()
+
+  if command == "change name":
     changeBTLocalName(getBtLocalName())
+
+  if command == "close":
+    break
+
+print("".center(45, "="), "\n", "".center(45, "-"))
+print("See you later ;)".center(45, "="))
+print("".center(45, "-"), "\n", "".center(45, "="))
